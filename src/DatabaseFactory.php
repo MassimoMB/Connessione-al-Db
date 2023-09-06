b4c2dc9745c33f72832adedd73a60cbac91d0480
Massimo <119443136+MassimoMB@users.noreply.github.com>
2023-09-06 17:47:47 +0200

    Refactory with dotEnv per configurazione Db.

--- src/DatabaseFactory.php
+++ src/DatabaseFactory.php
@@ -3,14 +3,18 @@
 namespace Massimo\Sakila2;
 
 
+use Dotenv\Dotenv;
 use Massimo\Sakila2\DatabaseContract;
 use Exception;
+use Massimo\Sakila2\DBConfig;
 use PDO;
 use PDOException;
 
 class DatabaseFactory{
-    public static function Create(DBConfig $dbConfig, string $type = DatabaseContract::TYPE_PDO): DatabaseContract | null{
+    public static function Create(string $type = DatabaseContract::TYPE_PDO): DatabaseContract | null{
 
+        $dbConfig= self::GetDBConfig();
+        
         if($type == DatabaseContract::TYPE_PDO)
         return self::CreateWithPDO($dbConfig);
         if($type == DatabaseContract::TYPE_MySQLi)
@@ -36,6 +40,7 @@ class DatabaseFactory{
     private static function CreateWithMySQLi(DbConfig $dbConfig): MySQLi{
 
         try{
+            
             $mysqli = new MySQLi($dbConfig);
                 
             return $mysqli;
@@ -46,4 +51,31 @@ class DatabaseFactory{
         }
 
     }
+
+    private static function GetDBConfig(): DBConfig{
+
+        $path = $_SERVER['DOCUMENT_ROOT'];
+        $dotenv= Dotenv::createImmutable($path);
+        $dotenv->load();
+        $dotenv->required(['DB_HOST', 'DB_NAME', 'DB_PORT', 'DB_USER', 'DB_PASS']);
+
+        $host= $_ENV['DB_HOST'];
+        $dbName= $_ENV['DB_NAME'];
+        $port= $_ENV['DB_PORT'];
+        $user= $_ENV['DB_USER'];
+        $pass= $_ENV['DB_PASS'];
+
+        return new DBConfig(
+            $host,
+            $dbName,
+            $port,
+            $user,
+            $pass
+        );
+
+
+
+    }
 }
+
+
